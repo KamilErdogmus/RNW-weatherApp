6427e80a172b03bbf22a4f85225b8e2bf8ddc3ad
kamilerdogmus96 <kamilerdogmsu96@gmail.com>
2024-12-09 07:49:27 +0300

    Loader changed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,22 @@
-import { View } from "react-native";
+// Loader.js
+import { View, Platform } from "react-native";
 import React from "react";
 import * as Progress from "react-native-progress";
 
+const WebLoader = () => (
+  <div className="flex items-center justify-center flex-1">
+    <div className="web-progress-circle" />
+  </div>
+);
+
+const NativeLoader = () => (
+  <View className="flex-row items-center justify-center flex-1">
+    <Progress.Circle thickness={10} size={40} color="#0bb1b2" />
+  </View>
+);
+
 const Loader = () => {
-  return (
-    <View className="flex-row items-center justify-center flex-1">
-      <Progress.Circle thickness={10} size={40} color="#0bb1b2" />
-    </View>
-  );
+  return Platform.OS === 'web' ? <WebLoader /> : <NativeLoader />;
 };
 
 export default Loader;
